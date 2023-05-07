a65317ca81edc7cec7a1f8902b446ebc0d8329a3
Wa <122730917+STKR2@users.noreply.github.com>
2023-05-08 02:39:38 +0300

    Update play.py

--- m8n/modules/play.py
+++ m8n/modules/play.py
@@ -60,7 +60,7 @@ from m8n.config import (
     BOT_USERNAME,
 )
 from m8n.utils.filters import command
-from m8n.utils.filters import command commandpro as command
+from m8n.utils.filters command import commandpro as command
 from m8n.utils.decorators import errors, sudo_users_only
 from m8n.utils.administrator import adminsOnly
 from m8n.utils.errors import DurationLimitError
