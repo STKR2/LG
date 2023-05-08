bd6ebf1f72f0d6bce5c9021ce93eb5d8908124c3
Wa <122730917+STKR2@users.noreply.github.com>
2023-05-08 13:25:01 +0300

    Update ytsearch.py

--- m8n/modules/ytsearch.py
+++ m8n/modules/ytsearch.py
@@ -19,21 +19,21 @@ logger = logging.getLogger(__name__)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
-@app.on_message(command(["search", "yt", "link"]))
+@app.on_message(command(["رابط", "اغنيه", "link"]))
 async def ytsearch(_, message: Message):
     try:
         if len(message.command) < 2:
-            await message.reply_text("`/search <keyword>` or `/yts <keyword>`")
+            await message.reply_text("‹ اكتب رابط واسم الاغنية للحصول على رابطها›")
             return
         query = message.text.split(None, 1)[1]
-        m = await message.reply_text("Searching....")
+        m = await message.reply_text("‹ يتم البحث الان ›")
         results = YoutubeSearch(query, max_results=7).to_dict()
         text = ""
         for i in range(4):
-            text += f"❂ **Title** - [{results[i]['title']}](https://youtube.com{results[i]['url_suffix']})\n"
-            text += f"❂ **Duration** - {results[i]['duration']}\n"
-            text += f"❂ **Views** - {results[i]['views']}\n"
-            text += f"❂ **Channel** - {results[i]['channel']}\n\n"
+            text += f" **‹ الاسم ›** - [{results[i]['title']}](https://youtube.com{results[i]['url_suffix']})\n"
+            text += f" **‹ الوصف ›** - {results[i]['duration']}\n"
+            text += f" **‹ عدد المشاهدات ›** - {results[i]['views']}\n"
+            text += f" **‹ قناة اليوتيوب ›** - {results[i]['channel']}\n\n"
         await m.edit(text, disable_web_page_preview=True)
     except Exception as e:
         await message.reply_text(str(e))
