8d258d00dbf80207d0603d79cbc3268e1eae6f07
Wa <122730917+STKR2@users.noreply.github.com>
2023-05-10 16:57:55 +0300

    Update extra.py

--- m8n/modules/extra.py
+++ m8n/modules/extra.py
@@ -105,7 +105,7 @@ async def showid(client, message):
 
 
 
-@@Client.on_message(command(["ا", "ايدي"]))
+@Client.on_message(command(["ا", "ايدي"]))
 async def who_is(client, message):
     """ - ايديك؟ """
     status_message = await message.reply_text(
