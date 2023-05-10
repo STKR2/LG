6c80bd4ee2eed8c0ce6ba8c82ccc022491c261cd
Wa <122730917+STKR2@users.noreply.github.com>
2023-05-10 17:17:34 +0300

    Update start.py

--- m8n/modules/start.py
+++ m8n/modules/start.py
@@ -3,9 +3,10 @@ import asyncio
 from pyrogram import Client, filters, __version__ as pyrover
 from pyrogram.errors import FloodWait, UserNotParticipant
 from pytgcalls import (__version__ as pytover)
+from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, ChatJoinRequest
 from m8n.utils.filters import command
-from m8n.utils.filters import command, other_filters
+
 
 from m8n.config import BOT_USERNAME
 from m8n.config import START_PIC
@@ -43,7 +44,7 @@ async def start_(client: Client, message: Message):
     )
 
 
-@Client.on_message(command(["المطور", f"مطور"]) filters.group & ~filters.edited)
+@Client.on_message(command("المطور", f"مطور") & filters.group & ~filters.edited)
 async def gcstart(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://te.legra.ph/file/5fdd8da2461c05d893189.jpg",
